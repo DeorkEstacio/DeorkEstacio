396b94bff07693b80e7b6cdb22d081ab8a7dea45
DeorkEstacio <202502020139@alunos.estacio.br>
2025-11-24 13:21:18 +0000

    Definindo a estrutura que representa um item

--- desafio_Free_Fire.c
+++ desafio_Free_Fire.c
@@ -4,3 +4,10 @@
 #include <string.h>
 
 #define MAX_ITENS 10 // número máximo de itens na mochila
+
+// Estrutura que representa um item
+typedef struct {
+    char nome[30];     // nome do item
+    char tipo[20];     // tipo do item (ex: alimento, ferramenta)
+    int quantidade;    // quantidade do item
+} Item;
