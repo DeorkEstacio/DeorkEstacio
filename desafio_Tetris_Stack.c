326099a9898cb76b87700cb22e3ef3bf445df0b9
DeorkEstacio <202502020139@alunos.estacio.br>
2025-11-26 13:42:36 +0000

    Criando a estrutura que representa uma peça

--- desafio_Tetris_Stack.c
+++ desafio_Tetris_Stack.c
@@ -2,3 +2,9 @@
 #include <stdlib.h>
 
 #define MAX 5 // Tamanho máximo da fila
+
+// Estrutura que representa uma peça
+typedef struct {
+    char tipo[2]; // 1 caractere + terminador nulo
+    int id;
+} Peca;
