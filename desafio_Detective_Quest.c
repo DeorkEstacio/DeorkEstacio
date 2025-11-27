bc2bec7ce83f644161df778e91144ff352d242b8
DeorkEstacio <202502020139@alunos.estacio.br>
2025-11-27 12:20:10 +0000

    Criando a estrutura de um local do mapa

--- desafio_Detective_Quest.c
+++ desafio_Detective_Quest.c
@@ -1,3 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+// Estrutura de um local do mapa
+typedef struct No {
+    char nome[50];          // Nome do local
+    struct No* esquerda;    // Caminho para esquerda
+    struct No* direita;     // Caminho para direita
+} No;
